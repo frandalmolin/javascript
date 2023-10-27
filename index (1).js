ae4f75149d2bb6fb5f2bfa0f7ea597cabbaa4e22
frandalmolin <137201084+frandalmolin@users.noreply.github.com>
2023-10-27 13:30:33 -0300

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,12 @@
+let numA = 4 ;
+let numB = 5 ;
+
+let soma = `A soma de ${numA} e ${numB} é ${numA + numB}.`
+let subtracao = `A subtração de ${numA} e ${numB} é ${numA - numB}.`
+let multiplicacao = `A multiplicação de ${numA} e ${numB} é ${numA * numB}.`
+let divisao = `A divisão de ${numA} e ${numB} é ${numA / numB}.`
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
